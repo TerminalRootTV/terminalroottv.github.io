e1077bd84ed5d0a5279b0211fb357c0b614a0c1e
Marcos Oliveira <marcos@terminalroot.com.br>
2026-01-02 15:45:59 -0300

    No message for this commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv){
   std::cout << "Terminal Root update./Gemfile(bundle update) 30 out 2025\n";
-  std::cout << "initpost.cpp!\n";
+  std::cout << "initpost.cpp teste!\n";
   return 0;
 }
 
